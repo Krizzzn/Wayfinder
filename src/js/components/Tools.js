379b0b5f8c5427c5a2056d17f688eaad6ff7813e
Christian Peterek <christian.peterek@gmail.com>
2016-11-15 22:49:41 +0100

    Adding a Floorplan selector

--- src/js/components/Tools.js
+++ src/js/components/Tools.js
@@ -2,10 +2,12 @@ import React from "react"
 import { connect } from "react-redux"
 
 import { toggleTool } from "../actions/toolsActions";
+import { changeFloorplan } from "../actions/floorplanActions";
 
 @connect((store) => {
   return {
-    tools: store.tools
+    tools:     store.tools,
+    floorplan: store.floorplan
   };
 })
 
@@ -15,19 +17,32 @@ export default class Layout extends React.Component {
     this.me.props.dispatch(toggleTool(this.tool))
   }
 
+  toggleFloorPlan(event){
+    this.props.dispatch(changeFloorplan(event.target.value));
+  }
+
   render() {
-  	const tools = this.props.tools;
+  	
+    const tools = this.props.tools;
+    const floors = this.props.floorplan.floors;
 
   	const snapActive = tools.snap ? 'active btn-primary' : ''
   	const dropNodes = tools.dropNodes ? 'active btn-primary' : ''
   	const selectPathMode = tools.selectPathMode ? 'active btn-primary' : ''
 
-    return <div class="topFloatForm">
+    const handleFloorplan = this.toggleFloorPlan.bind(this);
+    const floorOptions = floors.map(f => <option key={f.file} value={f.file}>{f.name}</option>)
+
+    return <div class="topFloatForm form-inline">
       			<button type="button" className={'btn btn-default ' + snapActive} onClick={this.toggleTools.bind({me: this, tool: 'snap'})}>Snap to grid (<kbd>x</kbd> or <kbd>z</kbd>)</button>
             &nbsp;
       			<button type="button" className={'btn btn-default ' + dropNodes} onClick={this.toggleTools.bind({me: this, tool: 'dropNodes'})}>Place nodes (<kbd>y</kbd>)</button> 
             &nbsp;
       			<button type="button" className={'btn btn-default ' + selectPathMode} onClick={this.toggleTools.bind({me: this, tool: 'selectPathMode'})}>Paths Mode (<kbd>s</kbd>)</button>
+            &nbsp;
+            |
+            &nbsp;
+            <select class="form-control" onChange={this.toggleFloorPlan.bind(this)}>{floorOptions}</select>
 			</div>
   }
 }
